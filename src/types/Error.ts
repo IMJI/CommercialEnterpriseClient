fea65c42d2e5b3de4d77e358fa2e290c0b70200a
IMJI <imjustice@yandex.ru>
2023-02-09 16:01:43 +0300

    Added isError function

--- src/types/Error.ts
+++ src/types/Error.ts
@@ -4,4 +4,10 @@ type Error = {
     message: string;
 }
 
+export function isError(obj: any): obj is Error {
+    return 'name' in obj
+        && 'status' in obj
+        && 'message' in obj
+}
+
 export default Error;
