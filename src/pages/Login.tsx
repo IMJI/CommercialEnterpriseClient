6829fdccdcec42ccee49e1e7acd8db5463bba0f2
IMJI <imjustice@yandex.ru>
2023-02-09 11:50:40 +0300

    Implementing login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import axios, { AxiosError } from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import useGlobalState from '../hooks/GlobalState';
+import useLocalStorage from '../hooks/LocalStorage';
+import LoginResponse from '../models/LoginResponse';
 
 function Login() {
+    const navigate = useNavigate();
+    const [user, setUser] = useLocalStorage('user', {});
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const login = async () => {
+        try {
+            const res = await axios.post('https://localhost:8080/login', {
+                email,
+                password
+            });
+            const data = (res.data) as LoginResponse;
+
+            setUser({
+                email: data.user.email,
+                token: data.token
+            });
+            navigate('/');
+        } catch (e) {
+            const error = e as AxiosError;
+            const errorRes = error.response?.data as Error;
+            setErrorMessage(errorRes.message);
+        }
+    }
+
     return (
         <div>
-            <input type="email" name="" id="" placeholder="Электронная почта" />
-            <input type="password" name="" id="" placeholder="Пароль" />
-            <button>Войти</button>
+            {errorMessage !== '' ? <p color='red'>{errorMessage}</p> : ''}
+            <input
+                type="email"
+                name=""
+                id="emailInput"
+                placeholder="Электронная почта"
+                onInput={e => setEmail((e.target as HTMLInputElement).value) }
+            />
+            <input
+                type="password"
+                name=""
+                id="passwordInput"
+                placeholder="Пароль"
+                onInput={e => setPassword((e.target as HTMLInputElement).value) }
+            />
+            <button onClick={login}>Войти</button>
         </div>
     );
 }
