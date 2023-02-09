400f55498f04e7218dc153e208568b241dd4163f
IMJI <imjustice@yandex.ru>
2023-02-09 11:50:28 +0300

    Added redirection if unauthorized

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import useGlobalState from '../hooks/GlobalState';
+import { Navigate } from 'react-router-dom';
+import useLocalStorage from '../hooks/LocalStorage';
+import User from '../models/User';
 
 type LayoutProps = {
     children?: React.ReactNode
 }
 
 function Layout({ children }: LayoutProps) {
+    const { state } = useGlobalState();
+    const [user, setUser] = useLocalStorage('user', {});
+
+    const isLoggedIn = !!(user && (user as User).token);
+
     return (
-        <div>
-            Layout: {children}
-        </div>
+        isLoggedIn ? (
+            <div>
+                <div>{(user as User).email}</div>
+                {children}
+            </div>
+          ) : (
+            <Navigate to='/login' />
+          )
     );
 }
 
