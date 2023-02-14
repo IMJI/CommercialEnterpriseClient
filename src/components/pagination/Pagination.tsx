efe721ea605676b389151eeb7440c2edddfd01af
IMJI <imjustice@yandex.ru>
2023-02-14 16:04:01 +0300

    Added pagination component

---
+++ src/components/pagination/Pagination.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Pagination.module.css';
+
+function Pagination() {
+    return (
+        <div className={classes.pagination}>
+            Pagination
+        </div>
+    );
+}
+
+export default Pagination;
