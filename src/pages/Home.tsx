bfbf7514e17b0c587a775f79195daf1c74421128
IMJI <imjustice@yandex.ru>
2023-02-14 16:05:59 +0300

    Basic layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Pagination from '../components/pagination/Pagination';
+import DataTable from '../components/table/DataTable';
+import classes from './Home.module.css';
 
 function Home() {
     return (
-        <div>
-            Home
+        <div className={classes.homePage}>
+            <div className={classes.container}>
+                <DataTable />
+            </div>
+            <Pagination />
         </div>
     );
 }
